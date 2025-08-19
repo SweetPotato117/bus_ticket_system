d2dddb464a95aed2f427a4f29b7991a794c8d87e
miggy <blasmiggy@gmail.com>
2025-08-19 21:48:38 +0800

    claude

--- models/dbconn.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '', 'bus_ticketing') or die('Connection Failed');
-//$conn = mysqli_connect('localhost', 'u187430566_bnhs', 'BNHSgamified_2025', 'u187430566_gamified') or die('Connection Failed');
